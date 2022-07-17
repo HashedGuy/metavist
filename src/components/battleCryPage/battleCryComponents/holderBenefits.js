14a55bcc92cabb3817cac2e085659919d3cde826
HashedGuy <hashedguy@gmail.com>
2022-07-17 13:50:04 +0400

    Metavist Cool Pulsing Edition

--- src/components/battleCryPage/battleCryComponents/holderBenefits.js
+++ src/components/battleCryPage/battleCryComponents/holderBenefits.js
@@ -1,85 +1,67 @@
 import { useState } from 'react'
 import smth from '../../../assets/img/vom.png'
+import MainGuy from '../../../assets/img/final/FablesMyth_BigGuy.png'
+import {motion} from 'framer-motion'
 
 function HolderBenefits(props) {
-  const [open, setOpen] = useState('')
+  const [open, setOpen] = useState('Intelligence')
   return (
-    <>
+    <div className='battleCrySection' ref={props.refG} id='vomRef'>
     <h1 className='valuesTitle' style={{textAlign:'center'}} >Values of Metavists</h1>
-    <div className='exp1Container demonContainer' ref={props.refG} id='vomRef'>
+    <div className='vomContainer' style={{backgroundImage: `url('https://ik.imagekit.io/74qyv5bswgr/Metavists/opacityAnime_OzJZW958S.png?ik-sdk-version=javascript-1.4.3&updatedAt=1658046343823)`}}>
+    <div className='exp1Container demonContainer' >
    
-   <div className='benefitCard' >
-     <div className='innerCard'>
-      <div className='frontCard'>
-       <h1>Intelligence</h1>
-       {/* <img src={Demon} className='coverImg'/> */}
-      </div>
-      <div className='backCard'>
-        <h1><a onClick={()=>setOpen('one')}>Intelligence</a></h1>
-        <p className='exp2Text'>You run on data and metrics. Realising the impact of an organisation lies in more than how it is marketed and the legacy it holds. </p>
-      </div>
-     </div>
+   <div className={open==='Generosity'? 'benefitCard activeBenefitCard': 'benefitCard'} onClick={()=>setOpen('Generosity')}>
+     Generous
+   </div>
+         
+ <div className={open==='Unity'? 'benefitCard downCard activeBenefitCard': 'benefitCard downCard'} onClick={()=>setOpen('Unity')}>
+  United
+ </div>
+</div>
+
+    <div className='exp1Container demonContainer'>
+   
+   <div className={open==='Intelligence'? 'benefitCard activeBenefitCard': 'benefitCard'} onClick={()=>setOpen('Intelligence')}>
+     Intelligent
    </div>
 
   <div className='demonImg'>
     <img className='demonImgZ' src={smth}/>
   </div>
 
-  <div className='benefitCard' >
-     <div className='innerCard'>
-      <div className='frontCard'>
-       <h1>Vision</h1>
-       {/* <img src={Demon} className='coverImg'/> */}
-      </div>
-      <div className='backCard'>
-        <h1><a onClick={()=>setOpen('one')}> Vision</a></h1>
-        <p className='exp2Text'>You believe bold ideas are the road to a brighter future. Blazing a path of your very own.</p>
-      </div>
-     </div>
-   </div>
+  <motion.div className='benefitText' >
+   <p>
+     {open==='Intelligence' ? 
+     'You run on data and metrics. Realising the impact of an organisation lies in more than how it is marketed and the legacy it holds.' : 
+     open==='Vision' ? 
+     'You believe bold ideas are the road to a brighter future. Blazing a path of your very own.' :
+     open==='Generosity' ? 
+     'You take heed in the idea that we are not what we take but what we give. Eager to share your knowledge and experience with others.' :
+     open==='Unity' ? 
+     'You believe that we are all in this together. You want to be able to lift all of us up together. Striving to know and respect the differences we hold as humans.' :
+     open==='Awareness' ? 
+     'You have your finger on the pulse of what’s happening. You want to make changes before they occur.'
+     : ''}</p>
+   </motion.div>
 </div>
 
-    <div className='exp1Container demonContainer' >
+<div className='exp1Container demonContainer' >
    
-      <div className='benefitCard' >
-        <div className='innerCard'>
-          <div className='frontCard'>
-          <h1>Generosity</h1>
-          {/* <img src={Demon} className='coverImg'/> */}
-          </div>
-          <div className='backCard'>
-            <h1><a onClick={()=>setOpen('one')}> Generosity</a></h1>
-            <p className='exp2Text'>You take heed in the idea that we are not what we take but what we give. Eager to share your knowledge and experience with others. </p>
-          </div>
-        </div>
-      </div>
-            
-    <div className='benefitCard downCard' >
-      <div className='innerCard'>
-        <div className='frontCard'>
-        <h1>Unity</h1>
-        {/* <img src={Demon} className='coverImg'/> */}
-        </div>
-        <div className='backCard'>
-          <h1><a onClick={()=>setOpen('one')}> Universal Unity</a></h1>
-          <p className='exp2Text'>You believe that we are all in this together. You want to be able to lift all of us up together. Striving to know and respect the differences we hold as humans.  </p>
-        </div>
-      </div>
-    </div>
-    <div className='benefitCard' >
-     <div className='innerCard'>
-      <div className='frontCard'>
-       <h1>Awareness</h1>
-       {/* <img src={Demon} className='coverImg'/> */}
-      </div>
-      <div className='backCard'>
-        <h1><a onClick={()=>setOpen('one')}>Awareness</a></h1>
-        <p className='exp2Text'>You have your finger on the pulse of what’s happening. You want to make changes before they occur. </p>
-      </div>
-     </div>
+   <div className={open==='Vision'? 'benefitCard activeBenefitCard': 'benefitCard'} onClick={()=>setOpen('Vision')}>
+     Visionary
    </div>
+         
+ <div className={open==='Awareness'? 'benefitCard downCard activeBenefitCard': 'benefitCard downCard'} onClick={()=>setOpen('Awareness')}>
+   Aware
  </div>
- </>
+
+</div>
+    </div>
+ 
+
+
+ </div>
   )
 }
 
